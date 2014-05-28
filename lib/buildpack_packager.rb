bb65b6c57e72e47b8251ce5444611ff169daeafc
Aaron Triantafyllidis, Eric Hu and Jordi Noguera <pair+atriantafyllidis+ehu+jordi@pivotallabs.com>
2014-05-28 11:47:19 -0400

    Introduce buildpack dependencies, use them while compiling

--- lib/buildpack_packager.rb
+++ lib/buildpack_packager.rb
@@ -3,7 +3,14 @@ require 'json'
 
 class BuildpackPackager < BasePackager
   def dependencies
-    []
+    [
+      'https://lang-php.s3.amazonaws.com/dist-beta/php-5.5.11.tar.gz',
+      'https://lang-php.s3.amazonaws.com/dist-beta/php-5.5.12.tar.gz',
+      'https://lang-php.s3.amazonaws.com/dist-beta/hhvm-3.0.1.tar.gz',
+      'https://lang-php.s3.amazonaws.com/dist-beta/apache-2.4.9.tar.gz',
+      'https://lang-php.s3.amazonaws.com/dist-beta/nginx-1.4.6.tar.gz',
+      'https://lang-php.s3.amazonaws.com/dist-beta/composer.tar.gz'
+    ]
   end
 
   def excluded_files
