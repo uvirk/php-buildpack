29dceff67a01a5f8e911e081260a95e5b04a9993
Aaron Triantafyllidis <pair+atriantafyllidis@pivotallabs.com>
2014-06-05 11:09:13 -0400

    HHVM test should ensure app is running on the VM
    [finishes #71465840]

--- cf_spec/fixtures/hhvm_web_app/index.php
+++ cf_spec/fixtures/hhvm_web_app/index.php
@@ -3,6 +3,12 @@
     <title>Simple</title>
 </head>
 <body>
-<?php echo '<p>App with HHVM running</p>'; ?>
+<?php
+  if (defined('HHVM_VERSION')) {
+    echo '<p>App with HHVM running</p>';
+  } else {
+    echo '<p>App without HHVM running</p>';
+  }
+?>
 </body>
 </html>
