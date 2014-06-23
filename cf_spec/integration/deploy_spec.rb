0dd7b3e7925cd6e67d63a1e781e66f9787f03750
Aaron Triantafyllidis and Kyle McConnaughay <pair+atriantafyllidis+kmcconnaughay@pivotallabs.com>
2014-06-23 10:57:26 -0400

    Use be_running matcher
    [#73544396]

--- cf_spec/integration/deploy_spec.rb
+++ cf_spec/integration/deploy_spec.rb
@@ -1,109 +1,127 @@
 $: << 'cf_spec'
-
 require 'spec_helper'
 
 describe 'CF PHP Buildpack' do
+  subject(:app) { Machete.deploy_app(app_name, options) }
+  let(:options) do
+    {}
+  end
 
   context 'with cached buildpack dependencies' do
-
     context 'in an offline environment', if: Machete::BuildpackMode.offline? do
+      context 'app has dependencies' do
+        let(:app_name) { 'app_with_local_dependencies' }
 
-      context 'an app with vendored app dependencies' do
-        it 'deploys' do
-          Machete.deploy_app("app_with_local_dependencies") do |app|
-            expect(app).to be_staged
-            expect(app.homepage_html).to include('App with dependencies running')
-            expect(app).to have_no_internet_traffic
-          end
+        specify do
+          expect(app).to be_running
+          expect(app.homepage_html).to include('App with dependencies running')
+          expect(app).to have_no_internet_traffic
+        end
+      end
+
+      context 'app has no dependencies' do
+        let(:app_name) { 'simple' }
+
+        specify do
+          expect(app).to be_running
+          expect(app.homepage_html).to include('Simple app running')
+          expect(app).to have_no_internet_traffic
+        end
+      end
+
+      context 'deploying a HHVM app' do
+        let(:app_name) { 'hhvm_web_app' }
+
+        specify do
+          expect(app).to be_running
+
+          logs = app.logs
+
+          expect(logs).to include('Detected request for HHVM 3.0.1')
+          expect(logs).to include('- HHVM 3.0.1')
+          expect(app.homepage_html).to include('App with HHVM running')
+          expect(app).to have_no_internet_traffic
+        end
+      end
+
+      context 'deploying a symfony app' do
+        let(:app_name) { 'symfony_hello_world_with_local_dependencies' }
+
+        specify do
+          expect(app).to be_running
+
+          expect(app.homepage_html).to include('Running on Symfony!')
+          expect(app).to have_no_internet_traffic
+        end
+      end
+
+      context 'deploying a wordpress app' do
+        let(:app_name) { 'composer_wordpress' }
+        let(:options) do
+          { with_pg: true, database_name: 'wordpress' }
         end
 
-        it 'deploys a HHVM app' do
-          Machete.deploy_app("hhvm_web_app") do |app|
-            expect(app).to be_staged
-
-            logs = app.logs
-
-            expect(logs).to include('Detected request for HHVM 3.0.1')
-            expect(logs).to include('- HHVM 3.0.1')
-            expect(app.homepage_html).to include('App with HHVM running')
-            expect(app).to have_no_internet_traffic
-          end
-        end
-
-        it 'deploys a symfony app' do
-          Machete.deploy_app("symfony_hello_world_with_local_dependencies") do |app|
-            expect(app).to be_staged
-
-            expect(app.homepage_html).to include('Running on Symfony!')
-            expect(app).to have_no_internet_traffic
-          end
-        end
-
-        it 'deploys a wordpress app' do
-          Machete.deploy_app("composer_wordpress", with_pg: true, database_name: "wordpress") do |app|
-            expect(app).to be_staged
-            expect(app).to have_no_internet_traffic
-          end
-        end
-
-        it 'deploys apps without dependencies' do
-          Machete.deploy_app("simple") do |app|
-            expect(app).to be_staged
-            expect(app.homepage_html).to include('Simple app running')
-            expect(app).to have_no_internet_traffic
-          end
+        specify do
+          expect(app).to be_running
+          expect(app).to have_no_internet_traffic
         end
       end
     end
   end
 
   context 'without cached buildpack dependencies' do
-
     context 'in an online environment', if: Machete::BuildpackMode.online? do
+      context 'app has dependencies' do
+        let(:app_name) { 'app_with_remote_dependencies' }
 
-      context 'an app with remote app dependencies' do
-        it 'deploys a PHP app' do
-          Machete.deploy_app("app_with_remote_dependencies") do |app|
-            expect(app).to be_staged
-            expect(app.homepage_html).to include('App with remote dependencies running')
-          end
+        specify do
+          expect(app).to be_running
+          expect(app.homepage_html).to include('App with remote dependencies running')
+        end
+      end
+
+      context 'app has no dependencies' do
+        let(:app_name) { 'simple' }
+
+        specify do
+          expect(app).to be_running
+          expect(app.homepage_html).to include('Simple app running')
+        end
+      end
+
+      context 'deploying a HHVM app' do
+        let(:app_name) { 'hhvm_web_app' }
+
+        specify do
+          expect(app).to be_running
+
+          logs = app.logs
+
+          expect(logs).to include('Detected request for HHVM 3.0.1')
+          expect(logs).to include('- HHVM 3.0.1')
+          expect(app.homepage_html).to include('App with HHVM running')
+        end
+      end
+
+      context 'deploying a symfony app' do
+        let(:app_name) { 'symfony_hello_world' }
+
+        specify do
+          expect(app).to be_running
+          expect(app.homepage_html).to include('Running on Symfony!')
+        end
+      end
+
+      context 'deploying a wordpress app' do
+        let(:app_name) { 'composer_wordpress' }
+        let(:options) do
+          { with_pg: true, database_name: 'wordpress' }
         end
 
-        it 'deploys a HHVM app' do
-          Machete.deploy_app("hhvm_web_app") do |app|
-            expect(app).to be_staged
-
-            logs = app.logs
-
-            expect(logs).to include('Detected request for HHVM 3.0.1')
-            expect(logs).to include('- HHVM 3.0.1')
-            expect(app.homepage_html).to include('App with HHVM running')
-          end
-        end
-
-        it 'deploys a symfony app' do
-          Machete.deploy_app("symfony_hello_world") do |app|
-            expect(app).to be_staged
-
-            expect(app.homepage_html).to include('Running on Symfony!')
-          end
-        end
-
-        it 'deploys a wordpress app' do
-          Machete.deploy_app("composer_wordpress", with_pg: true, database_name: "wordpress") do |app|
-            expect(app).to be_staged
-          end
-        end
-
-        it 'deploys apps without dependencies' do
-          Machete.deploy_app("simple") do |app|
-            expect(app).to be_staged
-            expect(app.homepage_html).to include('Simple app running')
-          end
+        specify do
+          expect(app).to be_running
         end
       end
     end
-
-    # context 'in an offline environment'
   end
 end
