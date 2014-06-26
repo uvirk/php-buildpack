708b5879c39a49b5110dfab139689b8f98d72765
Aaron Triantafyllidis and Kyle McConnaughay <pair+atriantafyllidis+kyle.mcconnaughay@pivotallabs.com>
2014-06-26 15:16:55 -0400

    Support for PHP 5.5.13 and HHVM 3.1.0

--- lib/buildpack_packager.rb
+++ lib/buildpack_packager.rb
@@ -6,7 +6,9 @@ class BuildpackPackager < BasePackager
     [
       'https://lang-php.s3.amazonaws.com/dist-beta/php-5.5.11.tar.gz',
       'https://lang-php.s3.amazonaws.com/dist-beta/php-5.5.12.tar.gz',
+      'https://lang-php.s3.amazonaws.com/dist-beta/php-5.5.13.tar.gz',
       'https://lang-php.s3.amazonaws.com/dist-beta/hhvm-3.0.1.tar.gz',
+      'https://lang-php.s3.amazonaws.com/dist-beta/hhvm-3.1.0.tar.gz',
       'https://lang-php.s3.amazonaws.com/dist-beta/apache-2.4.9.tar.gz',
       'https://lang-php.s3.amazonaws.com/dist-beta/nginx-1.4.6.tar.gz',
       'https://lang-php.s3.amazonaws.com/dist-beta/composer.tar.gz',
