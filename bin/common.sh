272986e49efae861ea140337dee7749dfde5ee12
Aaron Triantafyllidis & Rick Reilly <pair+azzamallow+rreilly@pivotal.io>
2014-09-22 10:59:12 -0400

    Revert "Merge remote-tracking branch 'upstream/master'"
    This reverts commit a68c6d5bfd401ae835115e9e2fc869a6fd87d7d5, reversing
    changes made to f7eb93303a405986b6a9a680cf9e8cafaedf925a.

--- bin/common.sh
+++ bin/common.sh
@@ -1,18 +1,22 @@
 error() {
   echo
-  echo " !     ERROR: $*" | indent no_first_line_indent
+  echo -n " !     ERROR: "
+  echo "$*" | indent
   echo
   exit 1
 }
 
 warning() {
   echo
-  echo " !     WARNING: $*" | indent no_first_line_indent
+  echo -n " !     WARNING: "
+  echo "$*" | indent
+  echo "See https://devcenter.heroku.com/categories/php" | indent
   echo
 }
 
 warning_inline() {
-  echo " !     WARNING: $*" | indent no_first_line_indent
+  echo -n " !     WARNING: "
+  echo "$*" | indent
 }
 
 status() {
@@ -22,6 +26,7 @@ status() {
 notice() {
   echo
   echo "NOTICE: $*" | indent
+  echo "See https://devcenter.heroku.com/categories/php" | indent
   echo
 }
 
@@ -33,8 +38,7 @@ notice_inline() {
 # so you get updates while the command runs and dont wait for the end
 # e.g. npm install | indent
 indent() {
-  # if an arg is given it's a flag indicating we shouldn't indent the first line, so use :+ to tell SED accordingly if that parameter is set, otherwise null string for no range selector prefix (it selects from line 2 onwards and then every 1st line, meaning all lines)
-  c="${1:+"2,999"} s/^/       /"
+  c='s/^/       /'
   case $(uname) in
     Darwin) sed -l "$c";; # mac/bsd sed: -l buffers on line boundaries
     *)      sed -u "$c";; # unix/gnu sed: -u unbuffered (arbitrary) chunks of data
